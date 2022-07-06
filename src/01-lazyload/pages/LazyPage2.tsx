2587b8a123ac19a07446ad627c67ef258bb1498c
Alexander-Aparicio <alexdel2022@gmail.com>
2022-07-06 12:40:05 -0500

    componentes para lazy agregados

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LazyPage2 = () => {
+  return <div>LazyPage1</div>;
+};
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LazyPage3 = () => {
+  return <div>LazyPage1</div>;
+};
